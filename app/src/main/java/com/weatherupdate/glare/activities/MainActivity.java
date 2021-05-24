a10da1ffa55e7b42038dbbeba6a301753fb3873c
tas-wtag <tasfia.seuti@welldev.io>
2021-05-24 16:19:15 +0600

    xml code cleanup

--- app/src/main/java/com/weatherupdate/glare/activities/MainActivity.java
+++ app/src/main/java/com/weatherupdate/glare/activities/MainActivity.java
@@ -4,8 +4,6 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -18,7 +16,6 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -55,7 +52,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     String longitudeOfSearchactivity;
     String latitudeOfCurrentLocation;
     String longitudeOfCurrentLocation;
-    MyWeatherData weatherData = new MyWeatherData ();
+    MyWeatherData weatherData = new MyWeatherData ( );
 
     private String timePause;
     public long dateInPause;
@@ -73,24 +70,23 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     TextView sunset;
     TextView wind_speed;
 
-    SharedPrefManager sharedPrefManager=new SharedPrefManager (this);
-    SharedPrefManager sharedPrefManager2=new SharedPrefManager (this);
+    SharedPrefManager sharedPrefManager = new SharedPrefManager (this);
+    SharedPrefManager sharedPrefManager2 = new SharedPrefManager (this);
     protected LocationManager locationManager;
-
-
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
+        MenuInflater inflater = getMenuInflater ( );
 
-        inflater.inflate(R.menu.main_activity_bar, menu);
-        return super.onCreateOptionsMenu(menu);
+        inflater.inflate (R.menu.main_activity_bar, menu);
+        return super.onCreateOptionsMenu (menu);
     }
+
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+        switch (item.getItemId ( )) {
             case R.id.action_search:
                 Intent intent = new Intent (MainActivity.this, SearchActivity.class);
                 startActivity (intent);
@@ -105,15 +101,16 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 return true;
 
             default:
-                return super.onOptionsItemSelected(item);
+                return super.onOptionsItemSelected (item);
 
         }
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
-        checkLocationPermission ();
+        checkLocationPermission ( );
 
         country = findViewById (R.id.cityName);
         city = findViewById (R.id.city);
@@ -170,6 +167,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             }
         }
     }
+
     @Override
     public void onLocationChanged(Location location) {
         weatherData.setLatitudeCurrentLocation ((location.getLatitude ( )));
@@ -193,13 +191,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     protected void onResume() {
         super.onResume ( );
-        String mySp = sharedPrefManager.getSearchActivity ("MySP");
-        if(sharedPrefManager.getSearchActivity ("MySP")!=null)
-        {
-            latitudeOfSearchActivity =sharedPrefManager2.getSearchActivity ("latitude3");
-            longitudeOfSearchactivity =sharedPrefManager2.getSearchActivity ("longitude3");
+        if (sharedPrefManager.getSearchActivity ("MySP") != null) {
+            latitudeOfSearchActivity = sharedPrefManager2.getSearchActivity ("latitude3");
+            longitudeOfSearchactivity = sharedPrefManager2.getSearchActivity ("longitude3");
         }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission (this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission (this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
         if (ActivityCompat.checkSelfPermission (this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission (this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -207,24 +203,26 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         }
         locationManager.requestLocationUpdates (LocationManager.GPS_PROVIDER, 400, 1, this);
     }
+
     @Override
     protected void onPause() {
-        super.onPause();
-        Calendar calendar = Calendar.getInstance ();
-        dateInPause=calendar.getTimeInMillis ();
-        timePause=Long.toString (dateInPause);
+        super.onPause ( );
+        Calendar calendar = Calendar.getInstance ( );
+        dateInPause = calendar.getTimeInMillis ( );
+        timePause = Long.toString (dateInPause);
 
         sharedPrefManager.setPauseTime ("timePause", timePause);
         if (ActivityCompat.checkSelfPermission (this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.checkSelfPermission (this, Manifest.permission.ACCESS_COARSE_LOCATION);
         }
-        locationManager.removeUpdates(this);
+        locationManager.removeUpdates (this);
     }
 
     @Override
     protected void onStop() {
         super.onStop ( );
     }
+
     @Override
     protected void onStart() {
         super.onStart ( );
@@ -234,13 +232,13 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     protected void onRestart() {
         super.onRestart ( );
         Calendar calendar2 = Calendar.getInstance ( );
-        dateInResume=calendar2.getTimeInMillis ();
-        SharedPrefManager.getPauseTime ("MySharedPref", timePause) ;
-        if(SharedPrefManager.getPauseTime ("MySharedPref", timePause) !=null) {
+        dateInResume = calendar2.getTimeInMillis ( );
+        SharedPrefManager.getPauseTime ("MySharedPref", timePause);
+        if (SharedPrefManager.getPauseTime ("MySharedPref", timePause) != null) {
             String timePause2 = SharedPrefManager.getPauseTime ("timePause", timePause);
-            if(timePause2!=null) {
-                dateInPause2=parseLong (timePause2);
-                long d = dateInResume- dateInPause2;
+            if (timePause2 != null) {
+                dateInPause2 = parseLong (timePause2);
+                long d = dateInResume - dateInPause2;
                 if (d > 300000) {
                     Intent intent = new Intent (MainActivity.this, MainActivity.class);
                     finish ( );
@@ -251,89 +249,89 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
     @SuppressLint("SetTextI18n")
-    public void updateUI (MyWeatherData wd){
-        @SuppressLint("SimpleDateFormat") SimpleDateFormat format= new SimpleDateFormat ("hh:mma");
-        int findTimeZoneInt=Integer.parseInt (weatherData.getFindTimeZone ());
+    public void updateUI(MyWeatherData wd) {
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat format = new SimpleDateFormat ("hh:mma");
+        int findTimeZoneInt = Integer.parseInt (weatherData.getFindTimeZone ( ));
 
-        country.setText (weatherData.getCountry ());
+        country.setText (weatherData.getCountry ( ));
         city.setText (city_name);
 
+        Picasso.get ( ).load (OnlyConstants.IMG_URL + weatherData.getImg ( ) + ".png").into (imageView);
 
-        Picasso.get ().load (OnlyConstants.IMG_URL + weatherData.getImg () + ".png").into (imageView);
-
-        latitude.setText (weatherData.getLatitude () + "°  N ");
-        longitude.setText (weatherData.getLongitude () + "°  E ");
-        humidity.setText (weatherData.getHumidity () + " %");
+        latitude.setText (wd.getLatitude ( ) + "°  N ");
+        longitude.setText (wd.getLongitude ( ) + "°  E ");
+        humidity.setText (wd.getHumidity ( ) + " %");
 
 
-        int findSunriseInt=Integer.parseInt (weatherData.getSunrise ());
-        int sunriseToShowInt=findSunriseInt+findTimeZoneInt;
-        String sunriseToShow=Integer.toString(sunriseToShowInt);
-        long sunriseLong= Long.parseLong (sunriseToShow) * 1000;
-        Date sunriseFind = new Date(sunriseLong);
+        int findSunriseInt = Integer.parseInt (wd.getSunrise ( ));
+        int sunriseToShowInt = findSunriseInt + findTimeZoneInt;
+        String sunriseToShow = Integer.toString (sunriseToShowInt);
+        long sunriseLong = Long.parseLong (sunriseToShow) * 1000;
+        Date sunriseFind = new Date (sunriseLong);
         format.setTimeZone (TimeZone.getTimeZone ("GMT"));
-        sunrise.setText (format.format(sunriseFind));
+        sunrise.setText (format.format (sunriseFind));
 
 
-        int findSunsetInt=Integer.parseInt (weatherData.getSunset ());
-        int sunsetToShowInt=findSunsetInt+findTimeZoneInt;
-        temp.setText (weatherData.getTemprature () + " °C ");
-        String sunsetToShow=Integer.toString(sunsetToShowInt);
+        int findSunsetInt = Integer.parseInt (wd.getSunset ( ));
+        int sunsetToShowInt = findSunsetInt + findTimeZoneInt;
+        temp.setText (wd.getTemprature ( ) + " °C ");
+        String sunsetToShow = Integer.toString (sunsetToShowInt);
         long sunsetLong = Long.parseLong (sunsetToShow) * 1000;
-        Date sunsetFind = new Date(sunsetLong);
+        Date sunsetFind = new Date (sunsetLong);
         format.setTimeZone (TimeZone.getTimeZone ("GMT"));
-        sunset.setText (format.format(sunsetFind));
+        sunset.setText (format.format (sunsetFind));
 
-        pressure.setText (weatherData.getPressure () + "  hPa");
-        wind_speed.setText (weatherData.getWindSpeed () + "  km/h");
+        pressure.setText (wd.getPressure ( ) + "  hPa");
+        wind_speed.setText (wd.getWindSpeed ( ) + "  km/h");
     }
 
-    @SuppressLint("StaticFieldLeak")
     private class FetchData extends AsyncTask<String, Void, String> {
-        TextView text;
-        public FetchData(TextView textView){
-            this.text=textView;
+        TextView date_time;
+
+        public FetchData(TextView textView) {
+            this.date_time = textView;
         }
+
         @Override
         protected String doInBackground(String... params) {
-            Bundle extras = getIntent().getExtras();
+            Bundle extras = getIntent ( ).getExtras ( );
             if (extras != null) {
-                latitudeOfCurrentLocation =(extras.getString ("latitude3"));
-                longitudeOfCurrentLocation =(extras.getString ("longitude3"));
-            }
-            else if (!latitudeOfSearchActivity.equals ("")) {
+                latitudeOfCurrentLocation = (extras.getString ("latitude3"));
+                longitudeOfCurrentLocation = (extras.getString ("longitude3"));
+            } else if (!latitudeOfSearchActivity.equals ("")) {
                 if (!longitudeOfSearchactivity.equals ("")) {
                     latitudeOfCurrentLocation = (latitudeOfSearchActivity);
-                    longitudeOfCurrentLocation =(longitudeOfSearchactivity);
+                    longitudeOfCurrentLocation = (longitudeOfSearchactivity);
                 } else {
-                    latitudeOfCurrentLocation =(String.valueOf (weatherData.getLatitudeCurrentLocation ()));
-                    longitudeOfCurrentLocation =(String.valueOf (weatherData.getLongitudeCurrentLocation ()));
+                    latitudeOfCurrentLocation = (String.valueOf (weatherData.getLatitudeCurrentLocation ( )));
+                    longitudeOfCurrentLocation = (String.valueOf (weatherData.getLongitudeCurrentLocation ( )));
                 }
             } else {
-                latitudeOfCurrentLocation =(String.valueOf (weatherData.getLatitudeCurrentLocation ()));
-                longitudeOfCurrentLocation = (String.valueOf (weatherData.getLongitudeCurrentLocation ()));
+                latitudeOfCurrentLocation = (String.valueOf (weatherData.getLatitudeCurrentLocation ( )));
+                longitudeOfCurrentLocation = (String.valueOf (weatherData.getLongitudeCurrentLocation ( )));
             }
             String inputLine;
-            StringBuilder result = new StringBuilder();
+            StringBuilder result = new StringBuilder ( );
             try {
-                URL url = new URL ("https://api.openweathermap.org/data/2.5/weather?units=metric&lat="+ latitudeOfCurrentLocation +"&lon="+ longitudeOfCurrentLocation +"&appid=1ccb72c16c65d0f4afbfbb0c64313fbf");
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection ();
+                URL url = new URL ("https://api.openweathermap.org/data/2.5/weather?units=metric&lat=" + latitudeOfCurrentLocation + "&lon=" + longitudeOfCurrentLocation + "&appid=1ccb72c16c65d0f4afbfbb0c64313fbf");
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection ( );
                 httpURLConnection.setRequestMethod ("GET");
-                httpURLConnection.setDoOutput(false);
-                InputStream inputStream = httpURLConnection.getInputStream ();
-                BufferedReader  bufferedReader = new BufferedReader (new InputStreamReader (inputStream));
-                while ((inputLine = bufferedReader.readLine ()) != null) {
+                httpURLConnection.setDoOutput (false);
+                InputStream inputStream = httpURLConnection.getInputStream ( );
+                BufferedReader bufferedReader = new BufferedReader (new InputStreamReader (inputStream));
+                while ((inputLine = bufferedReader.readLine ( )) != null) {
                     result.append (inputLine);
                 }
             } catch (ProtocolException e) {
-                e.printStackTrace ();
+                e.printStackTrace ( );
             } catch (MalformedURLException e) {
-                e.printStackTrace ();
+                e.printStackTrace ( );
             } catch (IOException e) {
-                e.printStackTrace ();
+                e.printStackTrace ( );
             }
-            return result.toString ();
+            return result.toString ( );
         }
+
         @SuppressLint("SetTextI18n")
         @Override
         protected void onPostExecute(String aVoid) {
@@ -356,7 +354,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 weatherData.setLatitude (object4.getDouble ("lat"));
 
                 JSONObject object5 = jsonObject.getJSONObject ("coord");
-                weatherData.setLongitude ( object5.getDouble ("lon"));
+                weatherData.setLongitude (object5.getDouble ("lon"));
 
                 JSONObject object6 = jsonObject.getJSONObject ("main");
                 weatherData.setHumidity (object6.getInt ("humidity"));
@@ -374,31 +372,32 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 JSONObject object10 = jsonObject.getJSONObject ("wind");
                 weatherData.setWindSpeed (object10.getString ("speed"));
 
-                updateTime (text);
+                updateTime (date_time);
 
                 updateUI (weatherData);
 
-            } catch (JSONException  jsonException) {
-                jsonException.printStackTrace ();
+            } catch (JSONException jsonException) {
+                jsonException.printStackTrace ( );
             }
         }
 
         Runnable updater;
+
         @SuppressLint("SetTextI18n")
         void updateTime(TextView dateTime2) {
-            final Handler timerHandler = new Handler();
+            final Handler timerHandler = new Handler ( );
             updater = () -> {
-                Calendar calender = Calendar.getInstance();
-                int day=calender.get(Calendar.DATE);
-                int month=calender.get(Calendar.MONTH)+1;
-                int year=calender.get(Calendar.YEAR);
-                int hour=calender.get (Calendar.HOUR_OF_DAY);
-                int min=calender.get (Calendar.MINUTE);
-                int sec=calender.get(Calendar.SECOND);
-                dateTime2.setText(day+"-"+month+"-"+year+getString(R.string.newLine)+hour+":"+min+":"+sec);
-                timerHandler.postDelayed(updater,1000);
+                Calendar calender = Calendar.getInstance ( );
+                int day = calender.get (Calendar.DATE);
+                int month = calender.get (Calendar.MONTH) + 1;
+                int year = calender.get (Calendar.YEAR);
+                int hour = calender.get (Calendar.HOUR_OF_DAY);
+                int min = calender.get (Calendar.MINUTE);
+                int sec = calender.get (Calendar.SECOND);
+                dateTime2.setText (day + "-" + month + "-" + year + getString (R.string.newLine) + hour + ":" + min + ":" + sec);
+                timerHandler.postDelayed (updater, 1000);
             };
-            timerHandler.post(updater);
+            timerHandler.post (updater);
         }
     }
 }
